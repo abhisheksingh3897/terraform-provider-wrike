153a038bc344398aafbb272a25eefd69a182d179
abhisheksingh3897 <57108661+abhisheksingh3897@users.noreply.github.com>
2021-06-24 09:23:37 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -7,14 +7,16 @@ terraform {
 	}
 }
   
-  provider "wrike"{
-	  token =  "bearer eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjQ2NTAxODYsXCJpXCI6Nzc0MzU1NyxcImNcIjo0NjI2MjkwLFwidVwiOjEwNjI2NjMwLFwiclwiOlwiVVNcIixcInNcIjpbXCJXXCIsXCJGXCIsXCJJXCIsXCJVXCIsXCJLXCIsXCJDXCIsXCJEXCIsXCJNXCIsXCJBXCIsXCJMXCIsXCJQXCJdLFwielwiOltdLFwidFwiOjB9IiwiaWF0IjoxNjIxMTc5NTkyfQ.56vbcUlIBctouj49OcOQoID0ehSmq4DveZHjKX3J2jY"
+  provider "wrike" {
+	  client_id = "ZtrqchYX"
+    client_secret = "X9SLPR2tiZrsUmiqht1R0a8K6qpuYwrFaFBRJ2JikSJnIAPETv6cKFDvhJooBxU0"
+    refresh_token = "eyJ0dCI6InAiLCJhbGciOiJIUzI1NiIsInR2IjoiMSJ9.eyJkIjoie1wiYVwiOjQ3NTIzODUsXCJpXCI6NzgwMzc0OCxcImNcIjo0NjI3MTA4LFwidlwiOlwiXCIsXCJ1XCI6MTA4ODk1MDgsXCJyXCI6XCJVU1wiLFwic1wiOltcIk5cIl0sXCJ6XCI6W1wicnNoXCJdLFwidFwiOjE2MjQ1MDcwMzUwMDB9IiwiZXhwIjoxNjI0NTA3MDM1LCJpYXQiOjE2MjQ1MDM0MzV9.eJ7BZf1-hAHX9aQOJQh3gc7ccXI9C4WaSEZFClIuolI"
   }
   
   resource "wrike_user" "user"{
-    role="Collaborator"
-    external=true
-    email= "shubham@clevertap.com"
+    # role="Collaborator"
+    # external=true
+    email= "abhishek.singh3897.com"
   }
 
   # data "wrike_user" "user"{
@@ -25,7 +27,7 @@ terraform {
 	#   value=data.wrike_user.user
   # }
 
-  output "resourse_list"{
+  output "resourse_user"{
 	  value=wrike_user.user
   }
   
--- wrike/provider.go
+++ wrike/provider.go
@@ -2,6 +2,7 @@ package wrike
 
 import (
 	"terraform-provider-wrike/client"
+	"terraform-provider-wrike/token"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -9,10 +10,20 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
-			"token": {
+			"refresh_token": {
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("WRIKE_TOKEN", ""),
+				DefaultFunc: schema.EnvDefaultFunc("WRIKE_REFRESH_TOKEN", ""),
+			},
+			"client_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("WRIKE_CLIENT_ID", ""),
+			},
+			"client_secret": {
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("WRIKE_CLIENT_SECRET", ""),
 			},
 		},
 		DataSourcesMap: map[string]*schema.Resource{
@@ -26,7 +37,10 @@ func Provider() *schema.Provider {
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
-	token := d.Get("token").(string)
+	token, err := token.GenerateToken(d.Get("client_id").(string), d.Get("client_secret").(string), d.Get("refresh_token").(string))
+	if err != nil {
+		return nil, err
+	}
 	return client.NewClient(token), nil
 
 }
